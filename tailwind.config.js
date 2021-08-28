570e91519fbe19706aa72f3a549d2c40e9ac4916
Jussi Raunio <jussi.raunio@gmail.com>
2021-08-28 16:05:44 +0300

    Rename dist -> docs. Add title.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
    purge: {
       enabled: true,
-      content: ["./dist/**/*.html"],
+      content: ["./docs/**/*.html"],
    },
    darkMode: false, // or 'media' or 'class'
    theme: {
