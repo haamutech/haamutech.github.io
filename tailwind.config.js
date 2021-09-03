19b5a13cd80d0e0f5c8d43b5186df143725572cf
Jussi Raunio <jussi.raunio@gmail.com>
2021-09-03 07:10:28 +0300

    Refinment of the page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 module.exports = {
    purge: {
-      enabled: true,
       content: ["./docs/**/*.html"],
    },
    darkMode: false, // or 'media' or 'class'
    theme: {
       extend: {
          colors: {
+            brand: {
+               DEFAULT: "#04e8b9"
+            },
             shiny: {
                DEFAULT: "#a2c3a4"
             }
